66c466a285dd4b71993129674386d8cf48b20da9
Ermakov Petr <ermakov.pd@gmail.com>
2014-04-09 12:51:17 +0400

    solution 2

--- mlclass-ex2-005/mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2-005/mlclass-ex2/costFunctionReg.m
@@ -17,10 +17,9 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
-
-
-
+h = sigmoid(X * theta);
+J = 1 / m * sum(-y .* log(h) - (1 - y) .* log(1 - h)) + lambda / (2 * m) * sum(theta(2:length(theta)) .^ 2);
+grad = 1 / m * X' * (h - y) + lambda / m * [ 0; theta(2:length(theta)) ];
 
 % =============================================================
 
