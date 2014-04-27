7761553598100330cfa86eb3aff0592e3fb9c7a9
Ermakov Petr <ermakov.pd@gmail.com>
2014-04-28 00:15:51 +0400

    solution 4

--- mlclass-ex4-005/mlclass-ex4/nnCostFunction.m
+++ mlclass-ex4-005/mlclass-ex4/nnCostFunction.m
@@ -24,11 +24,6 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
-J = 0;
-Theta1_grad = zeros(size(Theta1));
-Theta2_grad = zeros(size(Theta2));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the code by working through the
@@ -39,6 +34,19 @@ Theta2_grad = zeros(size(Theta2));
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
 %
+
+y = eye(num_labels)(y, :);
+a1 = X;
+a1 = [ones(m, 1) X];
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(size(a2, 1), 1) a2];
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+J = 1 / m * sum(sum(-y .* log(a3) - (1 - y) .* log(1 - a3))) + ...
+       lambda / (2 * m) * (sum(sum(Theta1(:, 2:end) .^ 2)) + ...
+                                            sum(sum(Theta2(:, 2:end) .^ 2)));
+
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
@@ -53,6 +61,12 @@ Theta2_grad = zeros(size(Theta2));
 %         Hint: We recommend implementing backpropagation using a for-loop
 %               over the training examples if you are implementing it for the 
 %               first time.
+
+delta2 = a3 - y;
+delta1 = (delta2 * Theta2(:, 2:end)) .* sigmoidGradient(z2);
+Theta1_grad = (delta1' * a1) / m;
+Theta2_grad = (delta2' * a2) / m;
+
 %
 % Part 3: Implement regularization with the cost function and gradients.
 %
@@ -62,24 +76,8 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Theta1_grad += (lambda * [ zeros(size(Theta1)(1), 1) Theta1(:, 2:end) ]) / m;
+Theta2_grad += (lambda * [ zeros(size(Theta2)(1), 1) Theta2(:, 2:end) ]) / m;
 % -------------------------------------------------------------
 
 % =========================================================================
